16535625d0bd30308197e26916efdfe90694a97a
Shri Hari <66116689+BugsyBouyy@users.noreply.github.com>
2020-09-21 12:29:08 +1000

    Add files via upload

--- Iteration_2/js/main2.js
+++ Iteration_2/js/main2.js
@@ -101,14 +101,14 @@ jQuery(document).ready(function($) {
 });
 
 var arr = [];
-exercise = 0;
-diet = 0;
-health = 0;
-lifestyle = 0;
-impregnability = 0;
-hypertension = 0;
-heart = 0;
-copd = 0;
+var exercise;
+var diet ;
+var health ;
+var lifestyle;
+var impregnability;
+var hypertension = 0;
+var heart = 0;
+var copd = 0;
 var i1 = 0;
 var i2 = 0;
 var i3 = 0;
@@ -125,59 +125,69 @@ function q1(el) {
   i1++;
   if (i1 === 1) {
     var amount = el.getAttribute('value');
-    arr.push(amount);
-    el.style.background = '#56a5eb';
-    
+    el.style.background = '#f1c111';
+    arr[0] = amount;
+
     console.log(arr);
     if (arr[0] === '1') {
-      exercise += 100;
-      health += 14.28;
-      lifestyle += 25;
-      impregnability += 33.32;
+      exercise = 100;
+      health = 14.28;
+      lifestyle = 25;
+      impregnability = 33.32;
       hypertension += 4.16;
       heart += 3.125;
     } else if (arr[0] === '2') {
-      exercise += 75;
-      health += 10.71;
-      lifestyle += 18.75;
-      impregnability += 24.99;
+      exercise = 75;
+      health = 10.71;
+      lifestyle = 18.75;
+      impregnability = 24.99;
       hypertension += 8.33;
       heart += 6.25;
     } else if (arr[0] === '3') {
-      exercise += 50;
-      health += 7.14;
-      lifestyle += 12.5;
-      impregnability += 16.67;
+      exercise = 50;
+      health = 7.14;
+      lifestyle = 12.5;
+      impregnability = 16.67;
       hypertension += 12.5;
       heart += 9.375;
     } else if (arr[0] === '4') {
-      exercise += 25;
-      health += 3.57;
-      lifestyle += 6.25;
-      impregnability += 8.33;
+      exercise = 25;
+      health = 3.57;
+      lifestyle = 6.25;
+      impregnability = 8.33;
       hypertension += 16.67;
       heart = 12.5;
-      
     }
   }
-  
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q2(el) {
   i2++;
   if (i2 === 1) {
     var amount = el.getAttribute('value');
-    arr.push(amount);
-    el.style.backgroundColor = '#56a5eb';
+    
+    el.style.backgroundColor = '#057a8d';
+    el.style.color = '#fff';
+    arr[1] = amount;
+    document.querySelectorAll('.dummy').forEach(function (el) {
+      el.addEventListener('click', function () {
+        console.log(document.querySelectorAll('.dummy').forEach((e) => {
+          if (e !== this) {
+            el.style.color = 'red';
+          }
+        }));
+      });
+    });
 
     if (arr[1] === '1') {
-      diet += 50;
+      diet = 50;
       lifestyle += 25;
       impregnability += 33.3;
       hypertension += 4.16;
       heart += 3.125;
     } else if (arr[1] === '2') {
-      diet += 37.5;
+      diet = 37.5;
       lifestyle += 18.75;
       impregnability += 24.975;
       hypertension += 8.33;
@@ -196,14 +206,16 @@ function q2(el) {
       heart += 12.5;
     }
   }
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q3(el) {
   i3++;
   if (i3 === 1) {
     var amount = el.getAttribute('value');
-    arr.push(amount);
-    el.style.backgroundColor = '#56a5eb';
+    el.style.backgroundColor = '#f1c111';
+    arr[2] = amount;
+
     if (arr[2] === '1') {
       health += 7.14;
       hypertension += 16.67;
@@ -213,14 +225,17 @@ function q3(el) {
       health += 14.28;
     }
   }
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q4(el) {
   i4++;
   if (i4 === 1) {
     var amount = el.getAttribute('value');
-    arr.push(amount);
-    el.style.backgroundColor = '#56a5eb';
+    el.style.backgroundColor = '#057a8d';
+
+    arr[3] = amount;
+
     if (arr[3] === '1') {
       heart += 12.5;
       health += 7.14;
@@ -228,14 +243,16 @@ function q4(el) {
       health += 14.28;
     }
   }
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q5(el) {
   i5++;
   if (i5 === 1) {
     var amount = el.getAttribute('value');
-    arr.push(amount);
-    el.style.backgroundColor = '#56a5eb';
+    el.style.backgroundColor = '#f1c111';
+    arr[4] = amount;
+
     if (arr[4] === '1') {
       hypertension +=  16.66;
       health += 7.14;
@@ -244,14 +261,16 @@ function q5(el) {
       health += 14.28;
     }
   }
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q6(el) {
   i6++;
   if (i6 === 1) {
     var amount = el.getAttribute('value');
-    arr.push(amount);
-    el.style.backgroundColor = '#56a5eb';
+    el.style.backgroundColor = '#057a8d';
+
+    arr[5] = amount;
     if (arr[5] === '1') {
       hypertension += 16.66;
       health += 7.14;
@@ -259,14 +278,16 @@ function q6(el) {
       health += 14.28;
     }
   }
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q7(el) {
   i7++;
   if (i7 === 1) {
     var amount = el.getAttribute('value');
-    arr.push(amount);
-    el.style.backgroundColor = '#56a5eb';
+    el.style.backgroundColor = '#f1c111';
+
+    arr[6] = amount;
     if (arr[6] === '1') {
       heart += 12.5;
       health += 7.14;
@@ -274,14 +295,16 @@ function q7(el) {
       health += 14.28;
     }
   }
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q8(el) {
   i8++;
   if (i8 === 1) {
     var amount = el.getAttribute('value');
-    arr.push(amount);
-    el.style.backgroundColor = '#56a5eb';
+    el.style.backgroundColor = '#057a8d';
+
+    arr[7] = amount;
     if (arr[7] === '1') {
       lifestyle += 0;
       impregnability += 8.335;
@@ -305,14 +328,16 @@ function q8(el) {
       impregnability += 33.34;
     }
   }
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q9(el) {
   i9++;
   if (i9 === 1) {
     var amount = el.getAttribute('value');
-    arr.push(amount);
-    el.style.backgroundColor = '#56a5eb';
+    el.style.backgroundColor = '#f1c111';
+
+    arr[8] = amount;
     if (arr[8] === '1') {
       lifestyle += 25;
       diet += 40;
@@ -330,63 +355,68 @@ function q9(el) {
       diet += 50;
     }
   }
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q10(el) {
   i10++;
   if (i10 === 1) {
     var amount = el.getAttribute('value');
-    arr.push(amount);
-    el.style.backgroundColor = '#56a5eb';
-    if (arr[6] === '1') {
+    el.style.backgroundColor = '#057a8d';
+    arr[9] = amount;
+    if (arr[9] === '1') {
       heart += 12.5;
       health += 7.14;
-    } else if (arr[6] === '2') {
+    } else if (arr[9] === '2') {
       health += 14.28;
     }
   }
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
-var text1 = "您可能有患充血性心力衰竭（CHF）的风险。如何改善生活方式以此来改善您的状况: 戒烟。 吸烟是心脏病的主要危险因素，戒烟是最好的办法。有一个健康的饮食: 包括新鲜水果，蔬菜和全谷物。避免吃高脂肪和高钠的食品。控制饮酒量。严格控制血糖可以帮助降低患心脏病的风险。进行体育锻炼，定期运动并保持健康的体重。请浏览我们的症状说明网页去了解更多关于您的状况。请浏览我们的医生查找页面以查找医生。";
+var text1 = "<br>You maybe at risk of Congestive heart failure (CHF). <br><strong>What lifestyle changes you can include to improve your condition:</strong> <br>1. Stop smoking: Smoking is a major risk factor for heart disease. Quitting is the best way. <br>2. Eat a healthy diet: Include fresh fruits, vegetables and whole grains. Avoid eating a high fat and high sodium diet. <br>3. Limit your alcohol intake. <br>4. Tight blood sugar control can help reduce the risk of heart disease. <br>5. Be physical active: have regular exercise and maintain a healthy weight. <br>Navigate to our Symptom Explainer page to learn more about your condition. Navigate to our Doctor Finder page to find a doctor.";
 
-var text2 = "您可能有患高血压的风险。患有类似疾病的人也会患有高血压。您可以做什么: 吃健康饮食。多吃新鲜水果和蔬菜。多吃富含多种钾，纤维，蛋白质，低盐和饱和脂肪的食物。进行有规律的身体锻炼。我们建议您每天进行1小时的身体锻炼，例如散步和骑自行车。保持你的体重指数（BMI）。请勿吸烟：吸烟会导致血压升高，使您更容易罹患心脏病和中风。减少饮酒: 饮酒过多会增加血压。获得充足的睡眠: 长时间睡眠不足与心脏病，高血压和中风密切相关。请浏览我们的症状说明页面以了解有关您状况的更多信息。请浏览我们的医生查找页面以查找医生。"
+var text2 = "<br>You maybe at risk of hypertension. People who had similar conditions also suffered from Hypertension. <br><strong>What you could do:</strong> <br>1. Eat a Healthy Diet. Eat plenty of fresh fruits and vegetables. Have a variety of foods rich in potassium, fiber, protein and low in salt and saturated fat. <br>2. Have regular physical activity.Its recommend you engage in 1 hour of physical activity such as walking and bicycling everyday. Maintain your body mass index(BMI). <br> 3. Do not smoke: Smoking raises blood pressure and puts you at higher risk for heart attack and stroke. <br>4. Reduce Alcohol consumption: Drinking too much alcohol can raise your blood pressure. <br>5. Get enough sleep: Not getting enough sleep on a regular basis is linked to risk of heart disease, high blood pressure and stroke. <br>Navigate to our Condition Explainer page to learn more about your condition. Navigate to our Doctor Finder page to find a doctor."
 
-var text3 = "您可能处于患有慢性阻塞性肺疾病（COPD）的危险。 慢性阻塞性肺疾病是指引起气流阻塞和呼吸相关问题的一系列疾病。它包括肺气肿和慢性支气管炎。您可以采取哪些措施来改善自己的病情: 戒烟。避免吸烟和吸入其他空气污染物。咨询医生以进行肺部康复。咳嗽和气喘可以服用医生开的药物进行治疗。请浏览我们的症状说明页面以了解有关您的状况的更多信息。请浏览我们的医生查找页面以查找医生。"
+var text3 = "<br>You maybe at risk of Chronic obstructive pulmonary disease(COPD). COPD refers to a group of diseases that cause airflow blockage and breathing-related problems. It includes emphysema and chronic bronchitis. <br><strong>What you can do to improve your condition:</strong> <br>1. Quit smoking. Avoid Tobacco Smoke and other air pollutants. <br>2. Visit a doctor for pulmonary rehabilitation. Coughing and wheezing can be treated with suitable medication prescribed by a doctor. <br>3. Navigate to our Condition Explainer page to learn more about your condition. Navigate to our Doctor Finder page to find a doctor."
 
-var text4 = "您身体状况良好！做得好 ！健康生活方式的关键包括: 1.健康饮食- 健康的饮食包括大量食用新鲜水果，蔬菜，全谷物和蛋白质。限制食用含饱和脂肪或高钠的食品。2.健康的身体锻炼-定期进行某种形式的身体锻炼。日常锻炼包括跑步，慢跑和骑自行车等锻炼。每天运动30分钟或确保每周至少运动四个小时。3.健康体重- 保持18-25的体重指数（BMI）以保持健康。保持身体状况对您的健康确实很重要。4.适度饮酒或不饮酒- 适度饮酒量为每天5至5毫升。一杯酒含14毫升酒精。尽可能减少饮酒量，甚至避免饮酒。继续浏览我们的症状说明页面以了解有关潜在的可预防性疾病的更多信息以及如何避免这些疾病。"
+var text4 = "<br>You are in good shape ! Great work ! The key to a healthy lifestyle includes: <br>1. Healthy Diet - A healthy diet includes good consumption of fresh fruits, vegetables, whole grains and proteins. Restrict consumption of foods with saturated fats or high sodium to as minimum as possible. <br>2. Healthy Physical Activity - Regularly engage in some form of physical activity. Include exercises such as running, jogging and cycling as part of your daily routine. Exercise 30 minutes everyday or make sure you have at least four hours of physical exercise every week. <br>3. Healthy Body weight - Maintain a Body mass index(BMI) of 18 - 25 to stay healthy.Staying fit is really important to your health. <br>4. Moderate or no Alcohol intake - Moderate alcohol consumption is measured to be between 5 to 25 grams per day. One drink contains 14 grams of alcohol. Reduce the amount of alcohol consumption to occasional or even avoid consumption if possible. <br>Continue to our Condition Explainer page to learn more about potential preventable diseases and what you can do to avoid them."
 
-var text5 = "您的健康需要进步！健康生活方式的关键包括: 1.健康饮食- 健康的饮食包括大量食用新鲜水果，蔬菜，全谷物和蛋白质。限制食用含饱和脂肪或高钠的食品。2.健康的身体锻炼- 定期进行某种形式的身体锻炼。日常锻炼包括跑步，慢跑和骑自行车等锻炼。每天运动30分钟或确保每周至少运动四个小时。3.健康体重- 保持18-25的体重指数（BMI）以保持健康。保持身体状况对您的健康确实很重要。4.适度饮酒或不饮酒- 适度饮酒量为每天5至5毫升。一杯酒含14毫升酒精。尽可能减少饮酒量，甚至避免饮酒。继续浏览我们的症状说明页面以了解有关潜在的可预防性疾病的更多信息以及如何避免这些疾病。"
+var text5 = "<br><strong>Your health needs more work ! The key to a healthy lifestyle includes:</strong> <br>1. Healthy Diet - A healthy diet includes good consumption of fresh fruits, vegetables, whole grains and proteins. Restrict consumption of foods with saturated fats or high sodium to as minimum as possible. <br>2. Healthy Physical Activity - Regularly engage in some form of physical activity. Include exercises such as running, jogging and cycling as part of your daily routine. Exercise 30 minutes everyday or make sure you have at least four hours of physical exercise every week. <br>3. Healthy Body weight - Maintain a Body mass index(BMI) of 18 - 25 to stay healthy.Staying fit is really important to your health. <br>4. Moderate or no Alcohol intake - Moderate alcohol consumption is measured to be between 5 to 25 grams per day. One drink contains 14 grams of alcohol. Reduce the amount of alcohol consumption to occasional or even avoid consumption if possible. <br>Continue to our Condition Explainer page to learn more about potential preventable diseases and what you can do to avoid them."
+
+var text6 ="<br> Finish the quiz and get back here."
 
 function displayVar(variable) {
 
-  if (heart >= hypertension && heart >= copd && heart > 50) {
+  if (heart >= hypertension && heart >= copd && heart >= 50) {
     document.getElementById(variable).innerHTML = text1;
-  } else if (hypertension > heart && hypertension > copd && hypertension > 50) {
+  } else if (hypertension > heart && hypertension > copd && hypertension >= 50 ) {
     document.getElementById(variable).innerHTML = text2;
   } else if (copd > heart && copd > hypertension && copd >= 50) {
     document.getElementById(variable).innerHTML = text3;
-  } else if (lifestyle > 80) {
+  } else if (lifestyle >= 80) {
     document.getElementById(variable).innerHTML = text4;
-  } else {
+  } else if (lifestyle < 80) {
     document.getElementById(variable).innerHTML = text5;
+  } else {
+    document.getElementById(variable).innerHTML = text6;
   }
 
   var marksCanvas = document.getElementById("marksChart");
 
-  var marksData = {
-    labels: ['锻炼身体', '平衡膳食', '健康的', '生活方式', '不可渗透性'],
-    datasets: [{
-      label: "你的健康",
-      backgroundColor: "rgba(0,200,200,0.2)",
+var marksData = {
+  labels: ['锻炼身体', '平衡膳食', '健康的', '生活方式', '不可渗透性'],
+  datasets: [{
+    label: "你的分数",
+    backgroundColor: "rgba(0,200,200,0.2)",
+    data: [exercise, diet, health, lifestyle, impregnability]
+  }]
+};
 
-      data: [exercise, diet, health, lifestyle, impregnability]
-    }]
-  };
-
-  var radarChart = new Chart(marksCanvas, {
-    type: 'radar',
-    data: marksData,
-    options: {
+var radarChart = new Chart(marksCanvas, {
+  type: 'radar',
+  data: marksData,
+  options: {
       scale: {
           ticks: {
             beginAtZero: true,
@@ -396,7 +426,7 @@ function displayVar(variable) {
           }
       }
     }
-  });
+});
 
   var riskCanvas = document.getElementById("barChart");
   
@@ -404,7 +434,7 @@ function displayVar(variable) {
   var riskData = {
     labels: ['高血压', '心力衰竭', 'COPD'],
     datasets: [{
-      label: ['高血压', '心力衰竭', 'COPD'],
+      label: "你的风险",
       backgroundColor: ['cyan', 'orange', 'lightgreen'],
 
       data: [hypertension, heart, copd]
@@ -413,14 +443,14 @@ function displayVar(variable) {
 
   var barChart = new Chart(riskCanvas, {
     type: 'horizontalBar',
-    data: {
-      labels: ["您的风险"],
+       data: {
+         labels: ["你的风险"],
       datasets: [{
-         label: "高血压",
+        label: "高血压",
          backgroundColor: "#0c8195",
          data: [hypertension],
       },{
-         label: "心力衰竭",
+          label: "心力衰竭",
          backgroundColor: "#f89624",
          data: [heart],
       },{
@@ -441,15 +471,6 @@ function displayVar(variable) {
     }
 
   });
-
-  // exercise = 0;
-  // diet = 0;
-  // health = 0;
-  // lifestyle = 0;
-  // impregnability = 0;
-  // hypertension = 0;
-  // heart = 0;
-  // copd = 0;
 
 }
 
