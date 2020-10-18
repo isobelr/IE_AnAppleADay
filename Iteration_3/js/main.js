6edb8cb69907638ab1850226e0ce17ac085897a5
Shri Hari <66116689+BugsyBouyy@users.noreply.github.com>
2020-10-18 16:31:42 +1100

    Add files via upload

--- Iteration_3/js/main.js
+++ Iteration_3/js/main.js
@@ -101,12 +101,11 @@ jQuery(document).ready(function($) {
 });
 
 var arr = [];
-var exercise = 0;
-var diet = 0 ;
+var exercise;
+var diet ;
 var health ;
-var lifestyle = 0;
+var lifestyle;
 var impregnability;
-var diabetes = 0;
 var hypertension = 0;
 var heart = 0;
 var copd = 0;
@@ -138,25 +137,35 @@ function q1(el) {
     console.log(arr);
     if (arr[0] === '1') {
       exercise = 100;
-      lifestyle = 33.34;
-
+      health = 14.28;
+      lifestyle = 25;
+      impregnability = 33.32;
+      hypertension += 4.16;
+      heart += 3.125;
     } else if (arr[0] === '2') {
       exercise = 75;
-      lifestyle = 25;
-
+      health = 10.71;
+      lifestyle = 18.75;
+      impregnability = 24.99;
+      hypertension += 8.33;
+      heart += 6.25;
     } else if (arr[0] === '3') {
       exercise = 50;
-      lifestyle = 16.67;
-      diabetes = 9.375;
-
+      health = 7.14;
+      lifestyle = 12.5;
+      impregnability = 16.67;
+      hypertension += 12.5;
+      heart += 9.375;
     } else if (arr[0] === '4') {
       exercise = 25;
-      lifestyle = 8.335;
-      diabetes = 12.5;
-
+      health = 3.57;
+      lifestyle = 6.25;
+      impregnability = 8.33;
+      hypertension += 16.67;
+      heart = 12.5;
     }
   }
-  console.log(exercise, diet, lifestyle, diabetes);
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q2(el) {
@@ -170,25 +179,31 @@ function q2(el) {
 
     if (arr[1] === '1') {
       diet = 50;
-      lifestyle += 33.34;
-
+      lifestyle += 25;
+      impregnability += 33.3;
+      hypertension += 4.16;
+      heart += 3.125;
     } else if (arr[1] === '2') {
       diet = 37.5;
-      lifestyle += 25;
-
+      lifestyle += 18.75;
+      impregnability += 24.975;
+      hypertension += 8.33;
+      heart += 6.25;
     } else if (arr[1] === '3') {
       diet += 25;
-      lifestyle += 16.67;
-      diabetes += 9.375;
-
+      lifestyle += 12.5;
+      impregnability += 16.65;
+      hypertension += 12.5;
+      heart += 9.375;
     } else if (arr[1] === '4') {
       diet += 12.5;
-      lifestyle += 8.335;
-      diabetes += 12.5;
-
+      lifestyle += 6.25;
+      impregnability += 8.325;
+      hypertension += 16.66;
+      heart += 12.5;
     }
   }
-  console.log(exercise, diet, lifestyle, diabetes);
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q3(el) {
@@ -200,13 +215,15 @@ function q3(el) {
     arr[2] = amount;
 
     if (arr[2] === '1') {
-      diabetes += 12.5;
+      health += 7.14;
+      hypertension += 16.67;
+      heart += 12.5;
 
     } else if (arr[2] === '2') {
-      diabetes += 0;
+      health += 14.28;
     }
   }
-  console.log(exercise, diet, lifestyle, diabetes);
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q4(el) {
@@ -219,12 +236,13 @@ function q4(el) {
     arr[3] = amount;
 
     if (arr[3] === '1') {
-      diabetes += 12.5;
+      heart += 12.5;
+      health += 7.14;
     } else if (arr[3] === '2') {
-      diabetes += 0;
+      health += 14.28;
     }
   }
-  console.log(exercise, diet, lifestyle, diabetes);
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q5(el) {
@@ -237,12 +255,14 @@ function q5(el) {
     arr[4] = amount;
 
     if (arr[4] === '1') {
-      diabetes += 12.5;
+      hypertension +=  16.66;
+      health += 7.14;
+      copd += 50;
     } else if (arr[4] === '2') {
-      diabetes += 0;
+      health += 14.28;
     }
   }
-  console.log(exercise, diet, lifestyle, diabetes);
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q6(el) {
@@ -254,12 +274,13 @@ function q6(el) {
 
     arr[5] = amount;
     if (arr[5] === '1') {
-      diabetes += 12.5;
+      hypertension += 16.66;
+      health += 7.14;
     } else if (arr[5] === '2') {
-      diabetes += 0;
+      health += 14.28;
     }
   }
-  console.log(exercise, diet, lifestyle, diabetes);
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q7(el) {
@@ -271,13 +292,13 @@ function q7(el) {
 
     arr[6] = amount;
     if (arr[6] === '1') {
-      diabetes += 12.5;
-
+      heart += 12.5;
+      health += 7.14;
     } else if (arr[6] === '2') {
-      diabetes += 0;
+      health += 14.28;
     }
   }
-  console.log(exercise, diet, lifestyle, diabetes);
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 function q8(el) {
@@ -286,61 +307,129 @@ function q8(el) {
     var amount = el.getAttribute('value');
     el.style.backgroundColor = '#057a8d';
     el.style.color = '#fff';
-    arr[7] = amount;
-    if (arr[7] === '1') {
-      diabetes += 12.5;
-    } else if (arr[7] === '2') {
-      diabetes += 0;
+    arr[9] = amount;
+    if (arr[9] === '1') {
+      heart += 12.5;
+      health += 7.14;
+    } else if (arr[9] === '2') {
+      health += 14.28;
     }
   }
-  console.log(exercise, diet, lifestyle, diabetes);
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
-// function q9(el) {
-//   i9++;
-//   if (i9 === 1) {
-//     var amount = el.getAttribute('value');
-//     el.style.backgroundColor = '#057a8d';
-//     el.style.color = '#fff';
-//     arr[9] = amount;
-//     if (arr[9] === '1') {
-//       heart += 12.5;
-//       health += 7.14;
-//     } else if (arr[9] === '2') {
-//       health += 14.28;
-//     }
-//   }
-//   console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
-// }
+function q9(el) {
+  i9++;
+  if (i9 === 1) {
+    var amount = el.getAttribute('value');
+    el.style.backgroundColor = '#057a8d';
+    el.style.color = '#fff';
+    arr[9] = amount;
+    if (arr[9] === '1') {
+      heart += 12.5;
+      health += 7.14;
+    } else if (arr[9] === '2') {
+      health += 14.28;
+    }
+  }
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
+}
 
 
 function q10(el) {
   i10++;
   if (i10 === 1) {
     var amount = el.getAttribute('value');
+    el.style.backgroundColor = '#057a8d';
+	  el.style.color = '#fff';
+    arr[9] = amount;
+    if (arr[9] === '1') {
+      heart += 12.5;
+      health += 7.14;
+    } else if (arr[9] === '2') {
+      health += 14.28;
+    }
+  }
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
+}
+
+function q11(el) {
+  i11++;
+  if (i11 === 1) {
+    var amount = el.getAttribute('value');
+    el.style.backgroundColor = '#057a8d';
+    el.style.color = '#fff';
+    arr[9] = amount;
+    if (arr[9] === '1') {
+      heart += 12.5;
+      health += 7.14;
+    } else if (arr[9] === '2') {
+      health += 14.28;
+    }
+  }
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
+}
+
+function q12(el) {
+  i12++;
+  if (i12 === 1) {
+    var amount = el.getAttribute('value');
+    el.style.backgroundColor = '#057a8d';
+    el.style.color = '#fff';
+
+    arr[7] = amount;
+    if (arr[7] === '1') {
+      lifestyle += 0;
+      impregnability += 8.335;
+      hypertension += 16.66;
+      heart += 12.5;
+      copd += 50;
+    } else if (arr[7] === '2') {
+      lifestyle += 6.25;
+      impregnability += 16.67;
+      hypertension += 12.5;
+      heart += 9.375;
+      copd += 37.5;
+    } else if (arr[7] === '3') {
+      lifestyle += 12.5;
+      impregnability += 20;
+      hypertension += 8.33;
+      heart += 6.25;
+      copd += 25;
+    } else if (arr[7] === '4') {
+      lifestyle += 25;
+      impregnability += 33.34;
+    }
+  }
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
+}
+
+function q13(el) {
+  i13++;
+  if (i13 === 1) {
+    var amount = el.getAttribute('value');
     el.style.backgroundColor = '#f1c111';
     el.style.color = '#fff';
 
     arr[8] = amount;
     if (arr[8] === '1') {
-      lifestyle += 33.34;
-      diet += 40;
-
-    } else if (arr[8] === '2') {
       lifestyle += 25;
+      diet += 40;
+      // heart += 3.125;
+    } else if (arr[8] === '2') {
+      lifestyle += 6.25;
       diet += 25;
-
+      heart += 9.375;
     } else if (arr[8] === '3') {
-      lifestyle += 16.67;
+      lifestyle += 3;
       diet += 12.5;
-
+      heart += 12.5;
     } else if (arr[8] === '4') {
-      lifestyle += 8.335;
+      lifestyle += 25;
       diet += 50;
     }
   }
-  
-  console.log(exercise, diet, lifestyle, diabetes);
+  console.log(exercise, health, diet, lifestyle, impregnability, hypertension, heart, copd);
 }
 
 var text1 = "<br>You maybe at risk of Congestive heart failure (CHF). <br><strong>What lifestyle changes you can include to improve your condition:</strong> <br>1. Stop smoking: Smoking is a major risk factor for heart disease. Quitting is the best way. <br>2. Eat a healthy diet: Include fresh fruits, vegetables and whole grains. Avoid eating a high fat and high sodium diet. <br>3. Limit your alcohol intake. <br>4. Tight blood sugar control can help reduce the risk of heart disease. <br>5. Be physical active: have regular exercise and maintain a healthy weight. <br>Navigate to our Symptom Explainer page to learn more about your condition. Navigate to our Doctor Finder page to find a doctor.";
@@ -357,9 +446,9 @@ var text6 ="<br> Finish the quiz and get back here."
 
 
 
-
-
 function displayVar(variable) {
+
+  
 
   if (heart >= hypertension && heart >= copd && heart >= 50) {
     document.getElementById(variable).innerHTML = text1;
@@ -374,35 +463,135 @@ function displayVar(variable) {
   } else {
     document.getElementById(variable).innerHTML = text6;
   }
-  
-  var x = lifestyle;
-  var y = exercise;
-  var z = diet;
 
-  
-  var testCanvas = document.getElementById('test1');
+  var marksCanvas = document.getElementById("marksChart");
 
-  var mainChart = new RadialProgressChart(testCanvas, {
-    diameter: 120,
+var marksData = {
+  labels: ["exercise", "diet","health","lifestyle","impregnability"],
+  datasets: [{
+    label: "Your Score",
+    backgroundColor: "rgba(0,200,200,0.2)",
+    data: [exercise, diet, health, lifestyle, impregnability]
+  }]
+};
+
+var radarChart = new Chart(marksCanvas, {
+  type: 'radar',
+  data: marksData,
+  options: {
+      scale: {
+          ticks: {
+            beginAtZero: true,
+            max: 100,
+            min: 0,
+            stepSize: 25
+          }
+      }
+    }
+});
+
+  var riskCanvas = document.getElementById("barChart");
+  
+
+  var riskData = {
+    labels: ['hypertension', 'heart failure', 'COPD'],
+    datasets: [{
+      label: "Your Risks",
+      backgroundColor: ['cyan', 'orange', 'lightgreen'],
+
+      data: [hypertension, heart, copd]
+    }]
+  };
+
+  var data = [
+    { day: 'M' },
+    { day: 'T' },
+    { day: 'W' },
+    { day: 'T' },
+    { day: 'F' },
+    { day: 'S' },
+    { day: 'S' }
+  ];
+  var dateFormatter = d3.time.format("%A, %B %d, %Y");
+  var mainChart = new RadialProgressChart('.main', {
+    diameter: 130,
     series: [
-      { labelStart: '\uF106', value: z },
-      { labelStart: '\uF101', value: y },
-      { labelStart: '\uF105', value: x }
+      { labelStart: '\uF106', value: 50 },
+      { labelStart: '\uF101', value: 50 },
+      { labelStart: '\uF105', value: 50 }
     ]
   }
-  
   );
 
+  d3.select('.week').selectAll('li')
+    .data(data).enter()
+    .append('li').on('click', function (d) {
+      // Update active class, date and main chart
+      d3.selectAll('.circle').classed('active', false);
+      d3.select(this).select('.circle').classed('active', true);
+      d3.select('.date').text(d.date);
+      mainChart.update(d.series);
+    })
+    .append('div').attr('class', 'circle').text(function (d) {
+      return d.day;
+    })
+    .each(function (d, i) {
+      d.date = dateFormatter(getDate(i));
+      d.series = [getRandom(), getRandom(), getRandom()];
+      new RadialProgressChart(this.parentNode, {
+        diameter: 10,
+        stroke: {
+          width: 6,
+          gap: 1
+        },
+        series: d.series
+      });
+    });
+
+  // Return some chronological dates
+  function getDate(i) {
+    var date = new Date('2015-06-16');
+    date.setDate(date.getDate() + i);
+    return date;
+  }
+  
+  var barChart = new Chart(riskCanvas, {
+    type: 'horizontalBar',
+       data: {
+      labels: ["Your Risks"],
+      datasets: [{
+         label: "Hypertension",
+         backgroundColor: "#0c8195",
+         data: [hypertension],
+      },{
+         label: "Heart Failure",
+         backgroundColor: "#f89624",
+         data: [heart],
+      },{
+         label: "COPD",
+         backgroundColor: "#33c1cf",
+         data: [copd],
+      }]
+   },
+    options: {
+      scales: {
+        xAxes: [{
+          ticks: {
+            max: 100,
+            min: 0
+          }
+        }]
+      }
+    }
+
+    
+
+
+  });
 
 
 
 }
-
-
-
-
-
-
 
 
 
