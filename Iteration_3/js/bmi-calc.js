ec90d24efe952618c52cbb94f97145f83043dff5
isobel <irow0001@student.monash.edu>
2020-10-18 22:32:47 +1100

    update menu bars

--- Iteration_3/js/bmi-calc.js
+++ Iteration_3/js/bmi-calc.js
@@ -4,14 +4,15 @@
    var height = Number(document.getElementById("height").value);
    var weight = Number(document.getElementById("weight").value);
    var result = (weight / (height**2)) * 10000;
-   // document.getElementById("result").innerHTML = "Your BMI score is <br>: " + result;
 
 
         if(result < 18.5){
          document.getElementById("result").innerHTML = "<br><h2 class=\"mb\-\4\" data-aos=\"fade-up\">Your BMI score is:  <br></h2>" + Math.round(result);
           document.getElementById("resultexplanation").innerHTML = "<br>Your BMI is classified as Underweight<br>";
+          
           var el = document.getElementById("resultpic");
           el.innerHTML="<img src=\"images/result_underweight.svg\">";
+
         }else if (result < 25){
          document.getElementById("result").innerHTML = "<br><h2 class=\"mb\-\4\" data-aos=\"fade-up\">Your BMI score is:  <br></h2>" + Math.round(result);
           document.getElementById("resultexplanation").innerHTML = "<br>Your BMI is classified as Normal<br>";
@@ -30,8 +31,5 @@
         }else{
           document.getElementById("resultexplanation").innerHTML = "<br><h2 class=\"mb\-\4\" data-aos=\"fade-up\">Oops! Please enter your height and weight and then press submit.</h2>";
         }
-
-
-
    }
 
